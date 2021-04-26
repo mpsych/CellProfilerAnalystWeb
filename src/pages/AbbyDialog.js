064e81072fc0673896f8242b84311531db57959e
abbyyvan <499965776@qq.com>
2021-04-26 01:59:40 -0400

    ‘xxx’

---
+++ src/pages/AbbyDialog.js
@@ -0,0 +1,48 @@
+import React ,{useState} from 'react';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import Slide from '@material-ui/core/Slide';
+import CloseIcon from '@material-ui/icons/Close';
+
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+//this components is for pop-up dialog display the 'confusion_matrix_canvas' for Evaluation button
+export default function DialogForCanvas() {
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    
+  };
+
+  return (
+    <div >
+
+      {/* evaluate button -> open a dialog window */}
+      <Button variant="contained"  onClick={handleClickOpen}>Evaluate</Button>
+
+      <Dialog
+        open={open}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={handleClose}>
+
+        {/* close icon to exit the dialog window */}
+        <CloseIcon onClick = {handleClose} style={{position: 'absolute', right: '10px', top: '10px',}}></CloseIcon>  
+        
+        {/* a canvas with a id : "confusion_matrix_canvas" */}
+        <canvas width={400} height={400} id="confusion_matrix_canvas"></canvas>
+        
+      </Dialog>
+      
+       
+    </div>
+  );
+}
