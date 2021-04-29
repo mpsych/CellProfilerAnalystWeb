19b19a064434e5df74292e1da34b66e38204ee50
Emma K <emma.kelminson001@umb.edu>
2021-04-28 21:19:15 -0400

    imageprovider2

--- src/classes/TrainingTable.js
+++ src/classes/TrainingTable.js
@@ -3,11 +3,14 @@ import { DataTables } from "./DataTables";
 export default class TrainingTable extends DataTables  {
     constructor(data_lines, column_lines) {
         super(data_lines, column_lines)
-        this.column_lines = column_lines;
+        this.data_lines = column_lines;
     }
     getTrainingLabels() {
         return this.data_table.map(row_object => {
             return row_object['label'] === 'positive'? 1 : 0
         })
     }
+    getDataArray() {
+        return this.data_lines;
+    }   
 }
