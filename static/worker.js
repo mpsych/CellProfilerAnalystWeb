ffb4621c6e5d33bab9726aca0df609f16ddd3f38
YahtzeeRage <Yahiya.Hussain001@umb.edu>
2021-05-04 16:34:50 -0400

    worker changes

--- static/worker.js
+++ static/worker.js
@@ -1,25 +1,33 @@
 self.importScripts("https://cdn.jsdelivr.net/npm/papaparse@5.3.0/papaparse.min.js")
 self.initialized = false
 self.onmessage = async (event) => {
-    console.log("got here")
-    console.log(Papa)
+
 
     if (event.data.action === "init") {
+        console.log("initialized worker")
         const fileListObject = event.data.fileListObject
         Papa.parse(fileListObject[5], {
             dynamicTyping: true,
             fastMode: true,
-            complete: (results) =>{self.postMessage({
-                action: "init", objectData:results.data}); 
+            skipEmptyLines: true,
+            complete: (results) =>{
                 self.objectData = results.data; 
                 self.initialized = true
+                self.postMessage({action: "init"})
             }
         })
     }
     else if (event.data.action === "get_row" && self.initialized) {
-        self.postMessage({action: "get_row", row:self.objectData[35000]})
-    } else {
-        console.log("Error: invalid action nothing passed back or not initialized yet")
+        self.postMessage({action: "get_row", row: self.objectData[event.data.index], uuid: event.data.uuid})
+    } 
+    else {
+        if (self.initialized) {
+            console.log("Error: invalid action")
+        }
+        else {
+            console.log("Error: webworker not initialized yet")
+        }
+        
     }
     
 };
