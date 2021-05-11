7b1f5a38f3c92c938f47343995e660a1155749ee
sketh222 <baidakbella@gmail.com>
2021-05-11 17:07:30 -0400

    ‘UI’

--- src/pages/ScoreAllGraph.js
+++ src/pages/ScoreAllGraph.js
@@ -6,25 +6,22 @@ import { VictoryHistogram, VictoryChart, VictoryLine, VictoryBar } from 'victory
 function ScoreAllGraph(){
     const sampleHistogramData = [
         {x: 1},
+        {x: 1},
+        {x: 1},
         {x: 2},
-        {x: 6},
-        {x: 5.3},
-        {x: 4.44},
-        {x: 12}
+        {x: 2},
+        {x: 3}, 
+        {x:30}
       ];
     return(
         <div>
-            <VictoryChart
-                domainPadding={{ x: 30 }}
-                >
+            <VictoryChart>
                 <VictoryHistogram
                     style={{
-                    data: { fill: "#c43a31" },
-                    height: 200, 
-                    width: 200
+                    data: { fill: "#c43a31" }
                     }}
                     data={sampleHistogramData}
-
+                    bins={30}
                 />
                 </VictoryChart>
         </div>
